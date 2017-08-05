9f8453d5692a2c9222c85414ba214c0f491aeeb6
felipemoura <felipe@icarustech.com.br>
2017-08-05 10:42:23 -0300

    console.js fixes

---
+++ core/command/make-async-model.js
@@ -0,0 +1,28 @@
+const S = require('string');
+const fs = require('fs');
+
+const makeAsyncModel = async (args) => {
+  try {
+    console.log("Args: ", args);
+
+    //terms
+    let baseName = args.name;
+    let entity = S(baseName).capitalize().s;
+    let schema = args.schema === undefined ? "name: String" : args.schema;
+
+    //generate model template
+
+    let templateModel = await fs.readFileSync('core/command/template_commands/template_model');
+    templateModel = templateModel.toString();
+    templateModel = templateModel.replaceAll('[MODEL]', entity);
+    templateModel = templateModel.replaceAll('[SCHEMA]', schema);
+
+    await fs.writeFileSync('app/models/' + baseName + ".js", templateModel);
+    console.log('Command executed with success!');
+    return;
+  } catch (e) {
+    console.warn("ERROR:", e);
+    return;
+  }
+};
+module.exports = makeAsyncModel;
